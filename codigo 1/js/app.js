678982cf86d2090130ea29d399c5983828024507
Axel Mercado <joaxel.mercado@gmail.com>
2023-02-03 15:30:58 -0600

    Codigo 1

--- codigo 1/js/app.js
+++ codigo 1/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+
+const $n = document.querySelector('[name="name"]');
+const $b = document.querySelector('[name="blog"]');
+const $l = document.querySelector('[name="location"]');
 
 async function displayUser(username) { //faltaba async
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //Se necesita un metodo JSON
+  const data = await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+
+  //Se le agrega el signo $ porque la constante estaba mal especificada
+
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
